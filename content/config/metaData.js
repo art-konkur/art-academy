c962f19901743eac57a3534d198ecd50502a60fd
art-konkur <kushk.edu@gmail.com>
2025-02-26 05:48:32 +0530

    modular docusaurus.config.js
    modular and seperate in 4 section in config directory.
    1. metaData
    2. themeConfig.js
    3. themeAndPlugins
    4.deployConfig.js
    5.index.js
    and in config path add a themeConfig folder add in there 2 files 1. navbar 2. footer
    export.modules and import

---
+++ content/config/metaData.js
@@ -0,0 +1,8 @@
+// config/metaData.js
+module.exports = {
+    title: 'کوشک کنکورهنر',
+    tagline: 'عمارتی برای یادگیری هنر',
+    favicon: 'img/favicon.ico',
+    url: 'https://kushk.surge.sh/',
+    baseUrl: '/'
+  };
---
+++ content/config/themeConfig/navbar.js
@@ -0,0 +1,9 @@
+// config/themeConfig/navbar.js
+module.exports = {
+    title: 'کوشک کنکورهنر',
+    items: [
+      {to: '/docs', label: 'دانش نامه', position: 'left'},
+      {to: '/blog', label: 'بلاگ', position: 'left'},
+      {href: 'https://github.com', label: 'گیت‌هاب', position: 'right'},
+    ]
+  };
